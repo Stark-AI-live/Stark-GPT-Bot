62fcb09299b6e10fbd63f42cb8ad4b136de8c966
Stark AI <119601417+Stark-AI-live@users.noreply.github.com>
2023-06-25 06:58:11 +0530

    Removed Log Channel for users privacy purpose

--- app.py
+++ app.py
@@ -9,7 +9,6 @@ chatbot = starkchat.StarkChat()
 API_ID = "" 
 API_HASH = ""
 BOT_TOKEN = ""
-CHAT_ID = ""
 API_KEY = ""
 
 bot = Client("STARK", api_id=API_ID, api_hash=API_HASH, bot_token=BOT_TOKEN)
@@ -25,8 +24,6 @@ async def response(client, message):
             r = requests.get(f"https://api.gpt.starkai.live/api?message={question}&key={API_KEY}&client_id={user_id}")
             reply = r.json()["message"]
             await msg.edit(reply)
-            TEXT = f"**Question:** `{question}`\n**Response:** `{reply}`\n**Engine:** `API`"
-            await bot.send_message(chat_id=CHAT_ID,text=TEXT)
             return
         except Exception as e:
             print(e)
@@ -34,8 +31,6 @@ async def response(client, message):
                 r = requests.get(f"https://asia.api.gpt.starkai.live/api?message={question}&key={API_KEY}&client_id={user_id}")
                 reply = r.json()["message"]
                 await msg.edit(reply)
-                TEXT = f"**Question:** `{question}`\n**Response:** `{reply}`\n**Engine:** `API`"
-                await bot.send_message(chat_id=CHAT_ID,text=TEXT)
                 return
             except Exception as e:
                 print(e)
@@ -43,21 +38,17 @@ async def response(client, message):
                     r = requests.get(f"https://us-east.api.gpt.starkai.live/api?message={question}&key={API_KEY}&client_id={user_id}")
                     reply = r.json()["message"]
                     await msg.edit(reply)
-                    TEXT = f"**Question:** `{question}`\n**Response:** `{reply}`\n**Engine:** `API`"
-                    await bot.send_message(chat_id=CHAT_ID,text=TEXT)
                     return
                 except Exception as e:
                     print(e)
                     reply = chatbot.chat(question)
                     await msg.edit(reply)
-                    TEXT = f"**Question:** `{question}`\n**Response:** `{reply}`\n**Engine:** `Library`"
-                    await bot.send_message(chat_id=CHAT_ID,text=TEXT)
                     return
 
 
 def main():
      bot.run()
-     bot.send_message(chat_id=CHAT_ID,text="Started...")
+
 
 
 if __name__ == "__main__":
